5adde50319bcef3a7018b85f8f05c8705673789d
thisisaaroland <aaron@mapzen.com>
2015-12-30 16:37:11 -0800

    start wiring in the IP lookup stuff

--- bin/start.py
+++ bin/start.py
@@ -29,6 +29,9 @@ if __name__ == '__main__':
     opt_parser.add_option('--data-port', dest='data_port', action='store', default='9999', help='The port to run the WOF data server on (default is 9999)')
     opt_parser.add_option('--www-host', dest='www_host', action='store', default='localhost', help='The host to run the WOF web server on (default is localhost)')
     opt_parser.add_option('--www-port', dest='www_port', action='store', default='8001', help='The port to run the WOF web server on (default is localhost)')
+    opt_parser.add_option('--iplookup-host', dest='iplookup_host', action='store', default='localhost', help='The host to run the WOF IP lookup server on (default is localhost)')
+    opt_parser.add_option('--iplookup-port', dest='iplookup_port', action='store', default='8668', help='The port to run the WOF IP lookup server on (default is localhost)')
+    opt_parser.add_option('--iplookup-data', dest='iplookup_data', action='store', default=None, help='The data source used by the WOF IP lookup server on (default is None which means it will look for a known datafile included with this repository)')
 
     options, args = opt_parser.parse_args()
 
@@ -53,6 +56,7 @@ if __name__ == '__main__':
 
     bin = os.path.join(root, "bin")
     www = os.path.join(root, "www")
+    data = os.path.join(root, "data")
 
     # Ensure config file
 
@@ -84,6 +88,7 @@ if __name__ == '__main__':
     pip_server = os.path.join(bin, "wof-pip-server")
     file_server = os.path.join(bin, "wof-fileserver")
     clone_tool = os.path.join(bin, "wof-clone-metafiles")
+    lookup_tool = os.path.join(bin, "wof-iplookup-server")
 
     # Do I need to pre-fetch any data?
 
@@ -106,21 +111,26 @@ if __name__ == '__main__':
     data_cmd = [file_server, "-cors", "-host", options.data_host, "-port", options.data_port, "-path", options.data]
     www_cmd = [file_server, "-host", options.www_host, "-port", options.www_port, "-path", www]
 
+    lookup_cmd = [lookup_tool, "-cors", "-host", options.iplookup_host, "-port", options.iplookup_port , "-mmdb", mmdb, "-concordances", concordances]
+
     logging.debug(" ".join(pip_cmd))
     logging.debug(" ".join(data_cmd))
     logging.debug(" ".join(www_cmd))
+    logging.debug(" ".join(lookup_cmd))
 
-    pip = subprocess.Popen(pip_cmd)
-    data = subprocess.Popen(data_cmd)
-    www = subprocess.Popen(www_cmd)
+    pip_process = subprocess.Popen(pip_cmd)
+    data_process = subprocess.Popen(data_cmd)
+    www_process = subprocess.Popen(www_cmd)
+    lookup_process = subprocess.Popen(lookup_cmd)
 
     # Watch for ctrl-C
 
     def signal_handler(signal, frame):
 
-        pip.terminate()
-        data.terminate()
-        www.terminate()
+        pip_process.terminate()
+        data_process.terminate()
+        www_process.terminate()
+        lookup_process.terminate()
 
         raise Exception, "all done"
 
